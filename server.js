d020d024bcdd1f8b7f737cda7da4f25e9a6d9bce
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-10 16:33:28 +0200

    Aktivate cors for localhost, Remove redundant Route - Remove route for start page. It's redundant because of used middleware for delivery of static files. - POST /saveMatchResult() is now reachable

--- server.js
+++ server.js
@@ -9,34 +9,38 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+// Importiert das CORS-Modul.
+import cors from 'cors';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Initialisiert eine Express-App.
 const app = express();
+const hostIP = '127.0.0.1';
 const port = 3000;
-
-app.use(express.json()); // for parsing application/json
-
-// Definiert eine Route für die Startseite ('/').
-// Wenn der Benutzer die Website ohne spezifischen Pfad aufruft, senden wir 'index.html'.
-let fileToDeliver = "index.html";
-app.get('/', (req, res) => {
-    console.log('User called website without path, Deliver index.html');
-    
-    // sendFile liefert die angegebene Datei an den Client. 
-    // __dirname ist das Verzeichnis, in dem das aktuelle Skript liegt.
-    res.sendFile(`${__dirname}/public/${fileToDeliver}`);
-});
 
 // Middleware zur Bereitstellung von statischen Dateien.
 // Alles in 'public' wird direkt zugänglich sein.
-// Wenn ein direkter Abruf eines Files manuell bearbeitet werden soll,
-// muss er vor dieser Zeile stehen.
 app.use(express.static('public'));
 
+// Aktiviert CORS nur für lokale Routen.
+app.use(cors({
+    // Erlaubt Anfragen nur von diesen Quelle
+    origin: function (origin, callback) {
+        console.log('CORS origin:', origin);
+         if (['http://127.0.0.1:3000', 'http://localhost:3000'].indexOf(origin) !== -1) {
+            callback(null, true)
+         } else {
+             callback(new Error('Nicht erlaubt durch CORS'))
+        }
+    }
+}));
+
+app.use(express.json()); // for parsing application/json
+
+let fileToDeliver = "index.html";
 let calledPath = '/one-player';
-fileToDeliver = "index.html";
 app.get(calledPath, (req, res) => {
     console.log(`User called Website with path ${calledPath}. We deliver ${fileToDeliver}`);
     res.sendFile(`${__dirname}/public/${fileToDeliver}`);
@@ -48,6 +52,6 @@ app.post('/saveMatchResult', (req, res) => {
 });
 
 // Startet den Server und lässt ihn auf dem definierten Port lauschen.
-app.listen(port, () => {
-  console.log(`Der Server ist unter http://localhost:${port} erreichbar.`);
+app.listen(port, hostIP, () => {
+    console.log(`Der Server ist unter http://${hostIP}:${port} erreichbar.`);
 });
