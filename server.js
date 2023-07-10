147c47285154c7e4afd484f70be4960b847ea8c3
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-10 14:32:18 +0200

    Fix Server uses paths of the project Fix GET support in server Add POST Route saveMatchResult to server (dummy, buggy)

--- server.js
+++ server.js
@@ -2,33 +2,47 @@
 
 // Name of this file: wordsworm.js
 
-// Importiert das Express-Modul für Webanwendungen.
+// Express-Modul für Webanwendungen.
 import express from 'express';
+
+// Pfade des Dateisystems bereitstellen
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // Initialisiert eine Express-App.
 const app = express();
-// Definiert den Port, auf dem der Server lauschen wird.
 const port = 3000;
 
 // Middleware zur Bereitstellung von statischen Dateien.
 // Alles in 'public' wird direkt zugänglich sein.
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.json()); // for parsing application/json
 
 // Definiert eine Route für die Startseite ('/').
 // Wenn der Benutzer die Website ohne spezifischen Pfad aufruft, senden wir 'index.html'.
+let fileToDeliver = "index.html";
 app.get('/', (req, res) => {
-    console.log('User called website without path, We deliver index.html');
-    // sendFile liefert die angegebene Datei an den Client. __dirname ist das Verzeichnis, in dem das aktuelle Skript liegt.
-    res.sendFile(`${__dirname}/public/index.html`);
+    console.log('User called website without path, Deliver index.html');
+    
+    // sendFile liefert die angegebene Datei an den Client. 
+    // __dirname ist das Verzeichnis, in dem das aktuelle Skript liegt.
+    res.sendFile(`${__dirname}/public/${fileToDeliver}`);
 });
 
-// Pfad und Datei zum Senden für einen bestimmten Anforderungspfad definieren.
+
 let calledPath = '/one-player';
-let fileToDeliver = "index.html";
-// Eine Route für 'calledPath' definieren.
+fileToDeliver = "index.html";
 app.get(calledPath, (req, res) => {
     console.log(`User called Website with path ${calledPath}. We deliver ${fileToDeliver}`);
-    // Sendet die angegebene Datei für Anfragen an 'calledPath'.
     res.sendFile(`${__dirname}/public/${fileToDeliver}`);
+});
+
+// ergänze ein post Route für saveMatchResult
+app.post('/saveMatchResult', (req, res) => {
+    console.log(`User called Server with path . We save a match result to the database`);
 });
 
 // Startet den Server und lässt ihn auf dem definierten Port lauschen.
