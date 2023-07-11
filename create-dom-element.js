73ccf42638cb587dce639d37ad371d81ed45b215
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-11 10:39:56 +0200

    Add helper code for creation of dom elements

---
+++ create-dom-element.js
@@ -0,0 +1,37 @@
+// create-dom-element.js
+
+'use strict';
+
+/* Usage:
+import createDOMElement from './createDOMElement.js';
+
+let el = createDOMElement({content: 'Hello', type: 'p'});
+*/
+
+const createDOMElement = ({
+    content = '',
+    type = 'div',
+    parent = ausgabe,
+    classes = [],
+    attr = {},
+    listeners = {},
+    styles = {},
+    amEnde = true,
+} = {}) => {
+    let neu = document.createElement(type);
+    if (content) neu.innerHTML = content;
+    if (classes.length) neu.className = classes.join(' ');
+
+    Object.entries(attr).forEach(el => neu.setAttribute(...el));
+    Object.entries(listeners).forEach(el => neu.addEventListener(...el));
+    Object.entries(styles).forEach(style => neu.style[style[0]] = style[1]);
+
+    if (!amEnde)  parent.prepend(neu);
+    else parent.append(neu);
+
+    return neu;
+}
+
+
+
+export default DOMElementAnlegen;
