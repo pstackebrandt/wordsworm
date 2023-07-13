660454e7ae85037eec097e6a9be71c66e8714cd4
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-13 23:12:55 +0200

    high scores page: style ui

--- public/js/high-scores.mjs
+++ public/js/high-scores.mjs
@@ -12,44 +12,63 @@ const highScoresModule = (() => {
             // Make sure the score list is empty before appending new scores.
             scoreList.innerHTML = '';
 
+            // Create the table element
+            let table = document.createElement('table');
+            table.classList.add('table', 'table-bordered');
+
+            // Create the table head
+            let thead = document.createElement('thead');
+            thead.classList.add('table-primary');
+
+            // Create the table head row
+            let headRow = document.createElement('tr');
+
+            // Create the team header cell
+            let teamHeader = document.createElement('th');
+            teamHeader.textContent = 'Team';
+
+            // Create the score header cell
+            let scoreHeader = document.createElement('th');
+            scoreHeader.textContent = 'Punkte';
+
+            // Append the header cells to the header row
+            headRow.appendChild(teamHeader);
+            headRow.appendChild(scoreHeader);
+
+            // Append the header row to the table head
+            thead.appendChild(headRow);
+
+            // Append the table head to the table
+            table.appendChild(thead);
+
+            // Create the table body
+            let tbody = document.createElement('tbody');
+
             data.forEach(score => {
-                // Create a new score row for each high score.
-                let scoreRow = document.createElement('div');
-                scoreRow.classList.add('score-row');
+                // Create a new row for each high score.
+                let row = document.createElement('tr');
 
-                // Create the team and score row.
-                let teamAndScoreRow = document.createElement('div');
-                teamAndScoreRow.classList.add('row');
-                let teamNameCol = document.createElement('div');
-                teamNameCol.classList.add('col');
-                teamNameCol.textContent = score.value.teamName;
-                let teamScoreCol = document.createElement('div');
-                teamScoreCol.classList.add('col');
-                teamScoreCol.textContent = score.value.teamScore;
-                teamAndScoreRow.appendChild(teamNameCol);
-                teamAndScoreRow.appendChild(teamScoreCol);
+                // Create the team cell
+                let teamCell = document.createElement('td');
+                teamCell.textContent = score.value.teamName;
 
-                // Create the found words row.
-                let foundWordsRow = document.createElement('div');
-                foundWordsRow.classList.add('row');
-                let foundWordsCol = document.createElement('div');
-                foundWordsCol.classList.add('col');
+                // Create the score cell
+                let scoreCell = document.createElement('td');
+                scoreCell.textContent = score.value.teamScore;
 
-                // Check if score.value.foundWords is defined and it's an array before joining
-                if (score.value.foundWords && Array.isArray(score.value.foundWords)) {
-                    foundWordsCol.textContent = score.value.foundWords.join(', ');
-                } else {
-                    // You can provide a fallback value here if foundWords is not defined or not an array
-                    foundWordsCol.textContent = 'Keine Wörter gefunden';
-                }
+                // Append the cells to the row
+                row.appendChild(teamCell);
+                row.appendChild(scoreCell);
 
-                // Append both rows to the score row.
-                scoreRow.appendChild(teamAndScoreRow);
-                scoreRow.appendChild(foundWordsRow);
-
-                // Append the score row to the score list.
-                scoreList.appendChild(scoreRow);
+                // Append the row to the table body
+                tbody.appendChild(row);
             });
+
+            // Append the table body to the table
+            table.appendChild(tbody);
+
+            // Append the table to the score list
+            scoreList.appendChild(table);
         } catch (error) {
             console.error('Failed to get high scores:', error);
         }
